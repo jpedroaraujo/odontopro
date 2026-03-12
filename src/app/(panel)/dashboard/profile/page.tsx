ef9477a3ddd5367f7d958e97005e1c94052924da
Joao Pedro <joao.pedro@iqnus.com.br>
2026-03-12 14:01:25 -0300

    add getUserData function for fetching user profile data and update Profile component

--- src/app/(panel)/dashboard/profile/page.tsx
+++ src/app/(panel)/dashboard/profile/page.tsx
@@ -1,7 +1,23 @@
-export default function Plans() {
+import getSession from "@/lib/getSession";
+import { redirect } from "next/navigation";
+import { getUserData } from "./_data-access/get-info-user";
+
+export default async function Profile() {
+  const session = await getSession();
+
+  if (!session) {
+    redirect("/");
+  }
+
+  const user = await getUserData({ userId: session.user.id });
+
+  if (!user) {
+    redirect("/");
+  }
+
   return (
     <div>
-      <h1>Plans</h1>
+      <h1>Profile</h1>
     </div>
   );
 }
